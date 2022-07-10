e9d93ef45763d8da4bed4e6e4b66c567518ef2fe
Lucas Machain <muyimacha@gmail.com>
2022-07-10 13:22:30 -0300

    update

---
+++ python/cursedhw.py
@@ -0,0 +1,12 @@
+# This is a cursed hello world 
+# Code taken from https://twitter.com/nedbat/status/1546151063903997954?s=12&t=L9_g8Y5iynWhQIF1_fbN8A
+
+@lambda _:_()
+class _:
+    def __format__(_, __):
+        _.__class__._ = property(lambda _: print(__))
+        return ""
+
+def __() -> f"{_:Hello, world!}": ...
+
+_._
